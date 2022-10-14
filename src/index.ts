95241bedfe6b25885a880cafd2828059d69fb9b7
ø Welcome ø <78008509+hungify@users.noreply.github.com>
2022-10-14 20:12:23 +0700

    refactor: hide total vistors and clean code

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 import fs from "fs";
-import { QUOTE_END_POINT, TYPING_EFFECT, VISITOR_COUNT } from "./constants";
+import { QUOTE_END_POINT, TYPING_EFFECT } from "./constants";
 import type { Quote } from "./interfaces";
 import { fetchData, getVietnameseDate } from "./utils";
 
-const totalVisitor = `<p align='center'><img src='${VISITOR_COUNT}'></p>`;
 const greetingEffect = `[![Typing SVG](${TYPING_EFFECT})](https://git.io/typing-svg)`;
 const images = `![RobotDance](src/assets/images/robot-dancing-dribble.gif?style=center)`;
 
 const makeQuote = async () => {
   try {
-    const quote = await fetchData<Quote>(QUOTE_END_POINT);
-    const content = quote?.content;
-    const author = quote?.author ? quote.author : "Anonymous";
+    const { content, author } = await fetchData<Quote>(QUOTE_END_POINT);
+    const newAuthor = author ?? "Anonymous";
 
     const today = getVietnameseDate(new Date());
-    const quoteToday = `${totalVisitor} \n\n${greetingEffect} \n _Quote Today (${today})_\n___\n>**_${content}_**\n___\n\n## __**_${author}_**\n\n${images}\n`;
+    const quoteToday = `${greetingEffect} \n _Quote Today (${today})_\n___\n>**_${content}_**\n___\n\n## __**_${newAuthor}_**\n\n${images}\n`;
 
-    fs.writeFileSync("README.md", quoteToday);
+    await fs.readFileAsync("README.md", quoteToday);
   } catch (error) {
     console.log(error);
   }
